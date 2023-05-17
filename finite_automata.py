7b53faad78f683a91d1d248f76755eab68d264c3
AlissonFS <alisson@MacBook-Air-de-Alisson.local>
2023-05-16 21:14:09 -0300

    WIP minimização: Lógica de retirar inalcançáveis e mortos funcionando

--- finite_automata.py
+++ finite_automata.py
@@ -14,6 +14,7 @@ class FiniteAutomata:
         self.__initial_state = initial_state
         self.__accept_states = accept_states
 
+
     def NFA_to_FA(self) -> None:
         transitions = self.__transitions
         # Calcula e-fecho e verifica se tem transições por epsilon.
@@ -37,6 +38,7 @@ class FiniteAutomata:
         # Define as novas transições.
         self.define_new_transitions(epsilon_closure, transitions)
 
+
     def calculate_epsilon_closure(self) -> tuple[dict, bool]:
         # Inicializa o e-fecho com os estados iniciais.
         epsilon_closure = self.initialize_epsilon_closure()
@@ -59,11 +61,13 @@ class FiniteAutomata:
                     epsilon_closure[state] = ','.join(states)
         return (epsilon_closure, True)
 
+
     def initialize_epsilon_closure(self) -> dict:
         epsilon_closure = {}
         for state in self.__states:
             epsilon_closure[state] = state
         return epsilon_closure
+
 
     def check_accept_state(self, transition: list) -> None:
         states = transition.split(',')
@@ -71,10 +75,12 @@ class FiniteAutomata:
             if state in self.__accept_states and transition not in self.__accept_states:
                 self.__accept_states.append(transition)
 
-    def clean_accept_states(self):
+
+    def clean_accept_states(self) -> None:
         for accept_state in self.__accept_states:
             if accept_state not in self.__states:
                 self.__accept_states.remove(accept_state)
+
 
     def define_new_transitions(self, epsilon_closure: dict, transitions: dict) -> None:
         for state in self.__states:
@@ -120,12 +126,13 @@ class FiniteAutomata:
         self.clean_accept_states()
 
 
-    def negate(self) -> None:
-        new_accept_states = []
-        for state in self.states:
-            if state not in self.__accept_states:
-                new_accept_states.append(state)
-        self.__accept_states = new_accept_states
+    # def negate(self) -> None:
+    #     new_accept_states = []
+    #     for state in self.states:
+    #         if state not in self.__accept_states:
+    #             new_accept_states.append(state)
+    #     self.__accept_states = new_accept_states
+
 
     def display(self) -> None:
         print('Estados: ', ' | '.join(self.__states))
@@ -135,6 +142,7 @@ class FiniteAutomata:
         print('Transições:')
         for state, transition in self.__transitions.items():
             print(f'{state} -> {" | ".join(transition)}')
+
 
     def export(self, filename: str):
         text = f'#FA\n#States\n' +\
@@ -154,21 +162,68 @@ class FiniteAutomata:
                 text += state + ' -> ' + ' | '.join(transition)
             else:
                 text += state + ' -> ' + ' | '.join(transition) + '\n'
-
-        # for transition in self.__transitions.values():
-        #     text += ' | '.join(transition) + '\n'
-
         with open(filename, 'w') as file:
             file.write(text)
 
-    def recognize(self, sentence):
+
+    # def recognize(self, sentence):
+    #     self.NFA_to_FA()
+    #     if not all([c in self.__alphabet for c in sentence]):
+    #         return False
+
+    #     current_state = self.__initial_state
+    #     for c in sentence:
+    #         i = self.__alphabet.index(c)
+    #         current_state = self.__transitions[current_state][i]
+
+    #     return current_state in self.__accept_states
+
+
+    def minimize(self) -> None:
+        # Determiniza.
         self.NFA_to_FA()
-        if not all([c in self.__alphabet for c in sentence]):
-            return False
+        # Remove estados inalcançáveis.
+        self.remove_unreachable_states()
+        # Remove estados mortos.
+        self.remove_dead_states()
+        # Junta estados equivalentes.
+        self.unite_equivalent_states()
 
-        current_state = self.__initial_state
-        for c in sentence:
-            i = self.__alphabet.index(c)
-            current_state = self.__transitions[current_state][i]
 
-        return current_state in self.__accept_states
+    def remove_unreachable_states(self) -> None:
+        reachable_states_list = [self.__initial_state]
+        for state in reachable_states_list:
+            new_reachable_states = []
+            transitions = self.__transitions[state]
+            for transition_state in transitions:
+                if ((transition_state not in reachable_states_list) and 
+                    (transition_state not in new_reachable_states) and 
+                    (transition_state != '-')):
+                    new_reachable_states.append(transition_state)
+            reachable_states_list.extend(new_reachable_states)
+
+        self.__states = list(set(self.__states).intersection(reachable_states_list))
+        self.__accept_states = list(set(self.__accept_states).intersection(reachable_states_list))
+        self.__transitions = {s: t for s, t in self.__transitions.items() if s in self.__states}
+
+
+    def remove_dead_states(self) -> None:
+        alive_states = self.__accept_states
+        for state in self.__states:
+            new_alive_states = []
+            transitions = self.__transitions[state]
+            for transition_state in transitions:
+                if ((transition_state in alive_states) and 
+                    (state not in alive_states) and 
+                    (state not in new_alive_states) and 
+                    (transition_state != '-')):
+                    new_alive_states.append(state)
+            alive_states.extend(new_alive_states)
+
+        self.__states = list(set(self.__states).intersection(alive_states))
+        self.__transitions = {s: t for s, t in self.__transitions.items() if s in alive_states}
+        self.__transitions = {s: [t if t in alive_states else '-' for t in ts] for s, ts in self.__transitions.items()}
+
+
+    def unite_equivalent_states(self) -> None:
+        pass
--- tests.py
+++ tests.py
@@ -4,15 +4,26 @@ from finite_automata import FiniteAutomata
 
 # Determinização.
 # Sem Epsilon.
-# file = File('NFA.txt')
-# automata = file.read_file()
+# file_NFA = File('automatas/NFA.txt')
+# automata = file_NFA.read_file()
 # automata.NFA_to_FA()
-# automata.export('FA.txt')
+# automata.export('automatas/FA.txt')
 # automata.display()
 
 # Com Epsilon.
-file = File('NFA_with_epsilon.txt')
-automata_with_epsilon = file.read_file()
-automata_with_epsilon.NFA_to_FA()
-automata_with_epsilon.export('FA_with_epsilon.txt')
-automata_with_epsilon.display()
+# file_NFA_with_epsilon = File('automatas/NFA_with_epsilon.txt')
+# automata_with_epsilon = file_NFA_with_epsilon.read_file()
+# automata_with_epsilon.NFA_to_FA()
+# automata_with_epsilon.export('automatas/FA_with_epsilon.txt')
+# automata_with_epsilon.display()
+
+
+# Minimização
+file_NFA_to_minimize = File('automatas/NFA_to_minimize.txt')
+automata_to_minimize = file_NFA_to_minimize.read_file()
+# automata_to_minimize.minimize()
+automata_to_minimize.NFA_to_FA()
+automata_to_minimize.remove_unreachable_states()
+automata_to_minimize.remove_dead_states()
+# automata_to_minimize.export('automatas/FA_minimum.txt')
+automata_to_minimize.display()
